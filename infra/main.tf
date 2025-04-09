66cf5ff6a214fc06fa95bf7b239add903e5a527e
Henrique Westphal <henriquewalves@gmail.com>
2025-04-09 17:24:36 -0300

    test

--- infra/main.tf
+++ infra/main.tf
@@ -1,17 +1,13 @@
 terraform {
   backend "s3" {
     bucket   = "dojo-management-tfstate"
-    key      = "dojo-lambda/${local.lambda_name}-tfstate"
+    key      = "dojo-lambda/dojo-athlete-mgmt-tfstate" ########
     region   = "sa-east-1"
     encrypt  = true
     skip_region_validation      = true
     skip_credentials_validation = true
     skip_metadata_api_check     = true
   }
-}
-
-locals {
-  lambda_name = "dojo-athlete-mgmt"
 }
 
 variable "env" {
@@ -22,7 +18,7 @@ variable "env" {
 module "lambda" {
   source = "git::https://github.com/DojoManagement/dojo-tf-modules.git//dojo-lambda?ref=dojo-lambda-0.0.1"
 
-  lambda_name     = local.lambda_name
+  lambda_name     = "dojo-athlete-mgmt" #####
   source_file     = "../app/lambda_function" #without extension
   handler         = "lambda_function.lambda_handler"
   runtime_version = "python3.12"
