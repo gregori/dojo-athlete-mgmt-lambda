e0221d8590dde9852ed8c7a1ff94116de6334b2c
Henrique Westphal <henriquewalves@gmail.com>
2025-05-24 12:51:16 -0300

    increase version of modules

--- infra/main.tf
+++ infra/main.tf
@@ -16,7 +16,7 @@ variable "env" {
 }
 
 module "lambda" {
-  source = "git::https://github.com/DojoManagement/dojo-tf-modules.git//dojo-lambda?ref=fix/deploy-lambda"
+  source = "git::https://github.com/DojoManagement/dojo-tf-modules.git//dojo-lambda?ref=dojo-lambda-0.0.2"
 
   lambda_name     = "dojo-athlete-mgmt" #####
   source_dir      = "../app"
