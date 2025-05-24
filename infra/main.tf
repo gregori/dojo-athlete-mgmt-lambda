dd4932a5236a2022d1a1ead89e37d9e062bc87db
Henrique Westphal <henriquewalves@gmail.com>
2025-05-24 12:46:49 -0300

    fix

--- infra/main.tf
+++ infra/main.tf
@@ -19,7 +19,7 @@ module "lambda" {
   source = "git::https://github.com/DojoManagement/dojo-tf-modules.git//dojo-lambda?ref=fix/deploy-lambda"
 
   lambda_name     = "dojo-athlete-mgmt" #####
-  source_file     = "../app/lambda_function" #without extension
+  source_dir      = "../app"
   handler         = "lambda_function.lambda_handler"
   runtime_version = "python3.12"
   access_s3       = true
