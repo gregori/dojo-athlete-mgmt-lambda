8abc5823bddd0612e393bcbb11dc78b9a91ae2e5
Rodrigo Gregori <spam@gregori.eti.br>
2025-07-07 22:19:27 -0300

    fix(repository): :fire: remove explicit call to create table

--- app/athletemgmt/repository/athlete_repository.py
+++ app/athletemgmt/repository/athlete_repository.py
@@ -3,8 +3,7 @@ from dojocommons.repository.base_repository import BaseRepository
 
 from athletemgmt.model.athlete import Athlete
 
+
 class AthleteRepository(BaseRepository[Athlete]):
     def __init__(self, cfg: AppConfiguration):
-        super().__init__(cfg, Athlete, "athletes")
-        # Garante que a tabela será criada a partir do Parquet no S3, se não existir
-        self._db.create_table_from_parquet("athletes")
+        super().__init__(cfg, Athlete, "athlete")
