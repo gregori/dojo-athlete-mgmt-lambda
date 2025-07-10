563f1bef2e96129c9009fd0db61dc8b1cb51108b
Henrique Westphal <henriquewalves@gmail.com>
2025-07-10 17:36:03 -0300

    test deploy

--- infra/main.tf
+++ infra/main.tf
@@ -26,7 +26,6 @@ module "lambda" {
   }
 }
 
-
 module "apigw_deployment" {
   source = "git::https://github.com/DojoManagement/dojo-tf-modules.git//dojo-apigw-deployment?ref=fix--create-one-api-gw-resource-by-path_part"
 }
