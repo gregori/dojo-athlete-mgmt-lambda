4b032641fea32309870c0c60e1713aa916f1e160
Henrique Westphal <henriquewalves@gmail.com>
2025-07-10 16:27:15 -0300

    test

--- infra/main.tf
+++ infra/main.tf
@@ -28,6 +28,4 @@ module "lambda" {
 
 module "apigw_deployment" {
   source = "git::https://github.com/DojoManagement/dojo-tf-modules.git//dojo-apigw-deployment?ref=dojo-apigw-deployment-0.0.1"
-
-  depends_on = [module.lambda]
 }
