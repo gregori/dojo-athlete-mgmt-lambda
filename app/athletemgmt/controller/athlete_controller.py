8b01b89ec2c4e2bd86f4dd1314180829b065c0d0
Henrique Westphal <henriquewalves@gmail.com>
2025-12-26 12:23:16 -0300

    fix

--- app/athletemgmt/controller/athlete_controller.py
+++ app/athletemgmt/controller/athlete_controller.py
@@ -1,6 +1,5 @@
 from dojocommons.controller.base_controller import BaseController
 from dojocommons.model.app_configuration import AppConfiguration
-
 from athletemgmt.model.athlete import Athlete
 from athletemgmt.model.resource import Resource
 from athletemgmt.service.athlete_service import AthleteService
@@ -8,4 +7,4 @@ from athletemgmt.service.athlete_service import AthleteService
 
 class AthleteController(BaseController[Athlete]):
     def __init__(self, cfg: AppConfiguration):
-        super().__init__(cfg, AthleteService,  Resource.ATTENDANCES.value, Athlete)
+        super().__init__(cfg, AthleteService, Resource.ATHLETES.value, Athlete)
